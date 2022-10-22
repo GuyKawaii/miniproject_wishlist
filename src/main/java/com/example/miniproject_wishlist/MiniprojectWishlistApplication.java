58975992809d1819a43a738ee70c48a11127e362
Daniel Alexander Dam <dani36i9@stud.kea.dk>
2022-10-22 20:30:01 +0200

    boilerplate

---
+++ src/main/java/com/example/miniproject_wishlist/MiniprojectWishlistApplication.java
@@ -0,0 +1,13 @@
+package com.example.miniproject_wishlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniprojectWishlistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniprojectWishlistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/miniproject_wishlist/MiniprojectWishlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miniproject_wishlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniprojectWishlistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

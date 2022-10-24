5a8eed0f671af79848081a984bfc03de21b9ff63
Daniel Alexander Dam <dani36i9@stud.kea.dk>
2022-10-24 11:03:24 +0200

    add model classes

---
+++ src/main/java/com/example/miniproject_wishlist/model/Gift.java
@@ -0,0 +1,40 @@
+package com.example.miniproject_wishlist.model;
+
+public class Gift {
+    private int gID;
+    private String name;
+    private double price;
+    private String url;
+
+    public int getgID() {
+        return gID;
+    }
+
+    public void setgID(int gID) {
+        this.gID = gID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
---
+++ src/main/java/com/example/miniproject_wishlist/model/GiftList.java
@@ -0,0 +1,31 @@
+package com.example.miniproject_wishlist.model;
+
+public class GiftList {
+    private int gID;
+    private int email;
+    private String name;
+
+    public int getgID() {
+        return gID;
+    }
+
+    public void setgID(int gID) {
+        this.gID = gID;
+    }
+
+    public int getEmail() {
+        return email;
+    }
+
+    public void setEmail(int email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

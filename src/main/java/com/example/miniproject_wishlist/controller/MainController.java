57137394971d9e1861d0bdbc2574a18723fe1e23
daniellorenzen <dani141d@stud.kea.dk>
2022-10-24 11:44:34 +0200

    html sites for "index", "myListOfGifts" and "myWishlists" created

--- src/main/java/com/example/miniproject_wishlist/controller/MainController.java
+++ src/main/java/com/example/miniproject_wishlist/controller/MainController.java
@@ -2,6 +2,8 @@ package com.example.miniproject_wishlist.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.context.request.WebRequest;
 
 @Controller
 public class MainController {
@@ -11,4 +13,31 @@ public class MainController {
         return "index";
     }
 
+    @PostMapping("/create")
+    public String createUser(WebRequest dataFromForm) {
+        //Create user i database with name and email
+
+        return "myWishlists";
+    }
+
+    @PostMapping("/createWishlist")
+    public String createWishlist(WebRequest dataFromForm) {
+        //Create list in database under the user
+
+        return "myListOfGifts";
+    }
+
+    @PostMapping("/createGift")
+    public String createGift(WebRequest dataFromForm) {
+        //Create wish/gift in database under the correct wishlist
+
+        return "myListOfGifts";
+    }
+
+    @PostMapping("/find")
+    public String findWishlist(WebRequest dataFromForm) {
+        //Find list by id
+
+        return "";
+    }
 }
